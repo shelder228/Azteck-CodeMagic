d1b00026a5ab542cde7b5859b82bcca72120dab4
leshabl <shelder228@gmail.com>
2025-06-20 18:59:31 +0300

    1
    1

---
+++ Aztok CollapseTests/Aztok_CollapseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aztok_CollapseTests.swift
+//  Aztok CollapseTests
+//
+//  Created by Олексій on 20.06.2025.
+//
+
+import Testing
+@testable import Aztok_Collapse
+
+struct Aztok_CollapseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Aztok CollapseUITests/Aztok_CollapseUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Aztok_CollapseUITestsLaunchTests.swift
+//  Aztok CollapseUITests
+//
+//  Created by Олексій on 20.06.2025.
+//
+
+import XCTest
+
+final class Aztok_CollapseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
